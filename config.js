6e418d9655abf14b604b4566fdffad2d7cd5648a
Blazeblue-cypher <82416603+Blazeblue-cypher@users.noreply.github.com>
2023-03-13 13:02:59 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '518145163825159',
+        clientSecret: '71e84724afcf3dd7d78fdd304b1b48c7'
+    }
+}
